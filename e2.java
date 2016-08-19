9e99447d06cda9d4561af50c6fce5825c14552cb
DhivyaRajagopal <dhivyamathi400@gmail.com>
2016-08-20 05:12:51 +0530

    Create e2.java

---
+++ e2.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class guvi{
+public static void main(String args[]){
+String Str=new String("welcome to guvicodekata.com");
+System.out.println("The given string is:"+Str);
+System.out.print("The substring is:");
+System.out.println(Str.substring(10));
+}
+}
+
